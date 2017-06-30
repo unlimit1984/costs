a277f10614f4eaaa3167eb8861e53bf9f5e6890e
Vladimir_Vysokomorny <Vladimir_Vysokomornyi@epam.com>
2017-06-30 08:20:19 +0200

    Add repository and inmemory impl for Purchase

---
+++ src/test/java/com/javapro/costs/service/PurchaseServiceTest.java
@@ -0,0 +1,31 @@
+package com.javapro.costs.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vladimir_Vysokomorny on 29-Jun-17.
+ */
+public class PurchaseServiceTest {
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void getBetweenDateTimes() throws Exception {
+    }
+
+    @Test
+    public void update() throws Exception {
+    }
+
+    @Test
+    public void delete() throws Exception {
+    }
+
+}
