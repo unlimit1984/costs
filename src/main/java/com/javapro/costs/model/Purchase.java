ea7d73b84097f7bf009544b4884048ce4c797542
unlimit1984Mac <unlimit_rmc@mail.ru>
2017-06-30 08:20:19 +0200

    Add repository and inmemory impl for Purchase

--- src/main/java/com/javapro/costs/model/Purchase.java
+++ src/main/java/com/javapro/costs/model/Purchase.java
@@ -1,15 +1,22 @@
 package com.javapro.costs.model;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 /**
  * Created by Vladimir on 20.03.2017.
  */
+@Data
 public class Purchase {
     Integer id;
     BigDecimal money;
     LocalDate createdDate;
     String category;
     String comment;
+
+    public boolean isNew() {
+        return id != null;
+    }
 }
