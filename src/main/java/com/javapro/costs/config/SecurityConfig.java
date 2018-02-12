188afd7c3f2fa5e5b8b5d7467f93ca1b56a73267
unlimit1984Mac <unlimit_rmc@mail.ru>
2018-02-12 23:59:13 +0100

    Add java based Spring MVC skeleton project

--- src/main/java/com/javapro/costs/config/SecurityConfig.java
+++ src/main/java/com/javapro/costs/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.javapro.costs.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-//@ComponentScan()
-public class AppConfig {
+public class SecurityConfig {
 }
