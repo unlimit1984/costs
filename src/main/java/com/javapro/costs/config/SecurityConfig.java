57250409518604164a82926cfc4fc6b322e85ba5
artem <temadivnich@gmail.com>
2018-05-28 23:30:19 +0200

    [#11] Add security configuration with remember-me functionality

--- src/main/java/com/javapro/costs/config/SecurityConfig.java
+++ src/main/java/com/javapro/costs/config/SecurityConfig.java
@@ -1,7 +1,31 @@
 package com.javapro.costs.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("123").roles("ADMIN").build();
+
+        auth.inMemoryAuthentication().withUser(user);
+        auth.inMemoryAuthentication().withUser(admin);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/test/**").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/purchases/**").access("hasRole('ROLE_USER')")
+                .and().formLogin();
+    }
 }
---
+++ src/main/java/com/javapro/costs/controller/SecurityController.java
@@ -0,0 +1,29 @@
+package com.javapro.costs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SecurityController {
+
+    @RequestMapping("/login")
+    public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+                              @RequestParam(value = "logout", required = false) String logout) {
+        ModelAndView mav = new ModelAndView("login");
+        if (error != null) {
+            mav.addObject("error", "error");
+        }
+        if (logout != null) {
+            mav.addObject("logout", "logout");
+        }
+        return mav;
+    }
+
+    @RequestMapping("/logout")
+    public ModelAndView logout() {
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("", "");
+        return mav;
+    }
+
+}
